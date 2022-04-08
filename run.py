b7fda04ebf3f305405d18723a5d84fe16e17b23e
Estelindis <estelindis@gmail.com>
2022-04-08 13:11:42 +0000

    Add p_d function to implement a delay after printing text

--- run.py
+++ run.py
@@ -1,3 +1,23 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+# Project Portfolio 3 (Python): Double Agent
+# Developed and written by Siobhán Mooney, April 2022.
+# Code is for a terminal of 80 characters wide and 24 rows high.
+
+import time
+
+delay_time = 2
+
+
+def p_d(text):
+    """
+    p_d stands for "print, delay"
+    Print a line of text.
+    Delay before the next line is printed.
+    Standard delay, as stored in the global variable delay_time, is 2 seconds.
+    This can be quickened or slowed via Settings.
+    Adapted from a function by Deanna Carina:
+    https://github.com/DeannaCarina/StarTrekTimeLoop
+    """
+    print(text)
+    delay = delay_time
+    time.sleep(delay)
+
