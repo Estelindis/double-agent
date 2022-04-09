fdc04cdb1c52ed88a01f082296f5d4e8948562b6
Estelindis <estelindis@gmail.com>
2022-04-09 15:30:48 +0000

    Add more story text to opening_scene

--- run.py
+++ run.py
@@ -143,7 +143,7 @@ def show_briefing():
     p_d("All official business takes place in the Khell language.")
     p_d("Knowledge of Adari ways is disdained in the Imperial bureaucracy...")
     p_d("...so most infiltrators hide their interest in such things.\n")
-    p_d(f"But you, {name}... You’re different.")
+    p_d(f"But you, {NAME}... You’re different.")
     p_d("You feel that serving your people in secret...")
     p_d("...doesn’t mean openly disavowing their past.")
     p_d("On free days, you wear Adari clothes, not the Imperial style.")
@@ -228,7 +228,7 @@ def opening_scene():
     p_d("In the Imperium, only the sorcerous ruling class are granted names.")
     p_d("Everyone else, including the Prefect herself, has only a title.")
     p_d("Of course, your parents did name you in secret.")
-    p_d(f"In the privacy of your mind, you always call yourself {name}.")
+    p_d(f"In the privacy of your mind, you always call yourself {NAME}.")
     p_d("But the Khell must never know that.")
     p_d("How do you greet the Prefect?")
     opening_options = [
@@ -237,15 +237,15 @@ def opening_scene():
         "  3. Playfully."
     ]
     opening_answer = make_choice(opening_options)
-    if opening_answer == "1":
+    if opening_answer == "1":  # Respectfully.
         p_d("You bow. “Prefect. How may I serve?”")
         p_d("The Prefect nods. “You ask precisely the right question.")
         p_d("I called you here for a very important reason.”")
         # Prefect's Trust + 1
-    if opening_answer == "2":
+    if opening_answer == "2":  # Neutrally.
         p_d("You give a small nod. “Prefect.”")
         p_d("She meets your eyes directly. “I called you here for a reason.”")
-    if opening_answer == "3":
+    if opening_answer == "3":  # Playfully.
         p_d("“Prefect. This is my first nocturnal invitation to the Palace...")
         p_d("...Forgive me if I don't quite know what to do.” A slight smirk.")
         p_d("The Prefect frowns, unamused. “My time is precious, Adjunct...")
@@ -268,10 +268,10 @@ def opening_scene():
         "  3. “I cannot do this.”"
     ]
     advisor_answer = make_choice(advisor_options)
-    if advisor_answer == "1":
+    if advisor_answer == "1":  # Of course I accept.
         p_d("You think you glimpse a predatory gleam in her eyes.")
         p_d("“Excellent. I’m glad you didn’t disappoint me.”\n")
-    if advisor_answer == "2":
+    if advisor_answer == "2":  # Why me?
         p_d("“I hope you’re merely being polite,” she says.")
         p_d("“It should be obvious. You’re the only trustworthy servant...")
         p_d("...of the Imperium with the necessary knowledge and skills.")
@@ -279,7 +279,7 @@ def opening_scene():
         p_d("I can’t inflict such ruffians on Governor Ekkano.”")
         p_d("It’s clear that she means for you to play this role.")
         p_d("She won’t take no for an answer.\n")
-    if advisor_answer == "3":
+    if advisor_answer == "3":  # I cannot do this.
         p_d("She shakes her head. “You can. And you will.")
         p_d("As we are commanded, so we all must obey.”")
         p_d("She won’t take no for an answer.\n")
@@ -311,10 +311,10 @@ def opening_scene():
         "  3. “You’re asking me to be a spy. I won’t do that.”"
     ]
     spy_answer = make_choice(spy_options)
-    if spy_answer == "1":
+    if spy_answer == "1":  # I will do it.
         p_d("She nods. “Good. Your obedience will be rewarded.”")
         # Prefect's Trust + 1
-    if spy_answer == "2":
+    if spy_answer == "2":  # Will he know?
         p_d("“No,” she says flatly. Then, grimacing, she elaborates.")
         p_d("“He’s a proud man. And why not? Spells can handle most threats.")
         p_d("But not everything. Those concerns fall to the Runeguard.")
@@ -324,15 +324,43 @@ def opening_scene():
         p_d("“Keep your observations strictly secret.")
         p_d("Tell no one but me.”")
         spy_questioned = True
-    if spy_answer == "3":
+    if spy_answer == "3":  # I will not be a spy.
         p_d("Her lip curls in contempt. “Be careful what you say, Adjunct.")
         p_d("I have asked no such thing. My order is perfectly legitimate.”")
         spy_refused = True
         # Prefect's Trust - 1
     if spy_questioned:
-        print("Further questioned options")
+        print("What do you say?")  # Following spy answer 2
+        spy_query_options = [
+            "  1. “I fully understand. I will do as you command.”",
+            "  2. “You’re asking me to be a spy. I won’t do that.”"
+            ]
+        spy_query_answer = make_choice(spy_query_options)
+        if spy_query_answer == "1":  # I will do it.
+            p_d("She nods. “Good. Your obedience will be rewarded.”")
+            # Prefect's Trust + 1
+        if spy_query_answer == "2":  # I will not be a spy.
+            p_d("Her lip curls in contempt. “Mind what you say, Adjunct.")
+            p_d("I’ve asked no such thing. My order is perfectly legitimate.”")
+            spy_refused = True
+            # Prefect's Trust - 1
     if spy_refused:
-        print("Further refused options")
+        print("What do you say?")  # Following spy_answer 3, spy_query_answer 2
+        spy_refused_options = [
+            "  1. “Forgive me, Prefect. I misspoke. I’ll do as you command.”",
+            "  2. “I said I won’t do it.”"
+            ]
+        spy_refused_answer = make_choice(spy_refused_options)
+        if spy_refused_answer == "1":  # Accept after all.
+            p_d("She frowns. “See that you do. I’ll accept no mistakes.”")
+            # Prefect's Trust + 1 - or not?
+        if spy_refused_answer == "2":  # Continue to refuse.
+            p_d("She sighs. “What a shame. I hope you understand...")
+            p_d("...your career is over if you refuse.”")
+            spy_refuse_again = True
+            # Prefect's Trust - 1
+    if spy_refuse_again:
+        print("What do you do?")  # Following spy_refused_answer 2
     p_d("")
     p_d("")
 
@@ -378,13 +406,13 @@ def start_game():
             game_declined = True
         else:
             print("It’s a yes or no question.")
-    global name
+    global NAME
     name_chosen = False
     while not name_chosen and not game_declined:
-        name = get_string("Agent, what is your name?")
-        if name:
+        NAME = get_string("Agent, what is your name?")
+        if NAME:
             print("")
-            p_d(f"{name}, you come to the crossroads of your life.")
+            p_d(f"{NAME}, you come to the crossroads of your life.")
             p_d("Tread carefully or boldly. See where your steps take you.\n")
             name_chosen = True
     read_brief = False
